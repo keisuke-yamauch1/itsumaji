c270fedee1d4f3645126be69d8b982779960cfcf
Keisuke Yamauchi <keisuke.yamauchi@kechiiiiin.com>
2026-04-19 12:27:50 +0900

    feat: RSS バッチ処理の scheduled ハンドラを実装

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import episodes from './routes/episodes'
+import {parseRss} from "./rss/parser";
 
 const app = new Hono<{ Bindings: CloudflareBindings}>()
 
@@ -8,4 +9,24 @@ app.get('/', (c) => {
 })
 app.route('/episodes', episodes)
 
-export default app
+export default {
+  fetch: app.fetch,
+  async scheduled(event: ScheduledController, env: CloudflareBindings, ctx: ExecutionContext) {
+    ctx.waitUntil(syncRss(env))
+  }
+}
+
+async function syncRss(env: CloudflareBindings) {
+  const response = await fetch("https://rss.listen.style/p/itsumaji-radio/rss")
+  const xml = await response.text()
+  const episodes = parseRss(xml)
+
+  const statements = episodes.map((episode) => {
+    return env.itsumaji_db.prepare(
+        `INSERT OR IGNORE INTO episodes
+       (guid, title, description, published_at, duration, thumbnail_url)
+       VALUES (?, ?, ?, ?, ?, ?)`
+    ).bind(episode.guid, episode.title, episode.description, episode.published_at, episode.duration, episode.thumbnail_url)
+  })
+  await env.itsumaji_db.batch(statements)
+}
